f165d8c40c590efece6dd7b92525416d0330b83e
Maurice <mbeckman@impress.nl>
2022-07-25 22:02:47 +0200

    removes wrong build

--- NetBakery/My Project/AssemblyInfo.vb
+++ NetBakery/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.0.728")>
-<Assembly: AssemblyFileVersion("2.0.728")>
+<Assembly: AssemblyVersion("2.0.729")>
+<Assembly: AssemblyFileVersion("2.0.729")>
