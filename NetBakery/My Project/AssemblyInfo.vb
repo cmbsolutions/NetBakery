5afe0efb6fe9c46708d7fe05633619fb9c1f16aa
Maurice Beckman <mbeckman@impress.nl>
2022-10-31 14:18:51 +0100

    adds pgpdecrypttemplate

--- NetBakery/My Project/AssemblyInfo.vb
+++ NetBakery/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.2.852")>
-<Assembly: AssemblyFileVersion("2.2.852")>
+<Assembly: AssemblyVersion("2.2.853")>
+<Assembly: AssemblyFileVersion("2.2.853")>
